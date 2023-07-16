47715356c75db3f4fc5c3bbe4aa82c2a26d9af82
junichiyamasaki <yamasaki@econ.kobe-u.ac.jp>
2023-07-16 06:56:33 +0000

    check latex and R. R version is not fixed yet.

--- r/hello.r
+++ r/hello.r
@@ -1,8 +1,16 @@
 library(renv)
+library(sf)
 
 paste("Hello, world!")
 
 d <- data.frame(x = 1:10, y = 1:10)
 View(d)
 plot(d$x, d$y)
-title(main = "図1")
+title(main = "table1")
+
+
+fname <- system.file("shape/nc.shp", package="sf")
+fname
+## [1] "/home/runner/work/_temp/Library/sf/shape/nc.shp"
+nc <- st_read(fname)
+plot(nc)
